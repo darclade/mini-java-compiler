123d3e28486a271ea9163e2f7d78f31cc4413765
victorliafook <victorliafook@2f0d5e16-249a-11df-af61-714e88269918>
2010-03-29 04:59:54 +0000

    Ta funcionando, so nao tive tempo de testar muito!!

--- src/foo2/javinhaConstants.java
+++ src/foo2/javinhaConstants.java
@@ -21,48 +21,88 @@ public interface javinhaConstants {
   /** RegularExpression Id. */
   int LTHEN = 9;
   /** RegularExpression Id. */
-  int LENGHT = 10;
+  int EQUALS = 10;
   /** RegularExpression Id. */
-  int CLASS = 11;
+  int BOOLEAN = 11;
   /** RegularExpression Id. */
-  int PUBLIC = 12;
+  int STRING = 12;
   /** RegularExpression Id. */
-  int EXTENDS = 13;
+  int WHILE = 13;
   /** RegularExpression Id. */
-  int THIS = 14;
+  int IF = 14;
   /** RegularExpression Id. */
-  int NEW = 15;
+  int ELSE = 15;
   /** RegularExpression Id. */
-  int INTEGER_LITERAL = 16;
+  int LENGHT = 16;
   /** RegularExpression Id. */
-  int DIGIT = 17;
+  int CLASS = 17;
   /** RegularExpression Id. */
-  int BOOL = 18;
+  int RETURN = 18;
   /** RegularExpression Id. */
-  int ID = 19;
+  int STATIC = 19;
   /** RegularExpression Id. */
-  int LIT = 20;
+  int VOID = 20;
   /** RegularExpression Id. */
-  int INT = 21;
+  int MAIN = 21;
   /** RegularExpression Id. */
-  int LPAREN = 22;
+  int PUBLIC = 22;
   /** RegularExpression Id. */
-  int RPAREN = 23;
+  int EXTENDS = 23;
   /** RegularExpression Id. */
-  int LBRACK = 24;
+  int SYSTEM = 24;
   /** RegularExpression Id. */
-  int RBRACK = 25;
+  int OUT = 25;
   /** RegularExpression Id. */
-  int BOOLEAN = 26;
+  int PRINTLN = 26;
   /** RegularExpression Id. */
-  int WHILE = 27;
+  int THIS = 27;
   /** RegularExpression Id. */
-  int IF = 28;
+  int INT = 28;
   /** RegularExpression Id. */
-  int ELSE = 29;
+  int NEW = 29;
+  /** RegularExpression Id. */
+  int INTEGER_LITERAL = 30;
+  /** RegularExpression Id. */
+  int DIGIT = 31;
+  /** RegularExpression Id. */
+  int BOOL = 32;
+  /** RegularExpression Id. */
+  int ID = 33;
+  /** RegularExpression Id. */
+  int LIT = 34;
+  /** RegularExpression Id. */
+  int SCOLON = 35;
+  /** RegularExpression Id. */
+  int COMMA = 36;
+  /** RegularExpression Id. */
+  int DOT = 37;
+  /** RegularExpression Id. */
+  int LPAREN = 38;
+  /** RegularExpression Id. */
+  int RPAREN = 39;
+  /** RegularExpression Id. */
+  int LBRACK = 40;
+  /** RegularExpression Id. */
+  int RBRACK = 41;
+  /** RegularExpression Id. */
+  int LCHAVE = 42;
+  /** RegularExpression Id. */
+  int RCHAVE = 43;
+  /** RegularExpression Id. */
+  int SINGLE_LINE_COMMENT = 47;
+  /** RegularExpression Id. */
+  int FORMAL_COMMENT = 48;
+  /** RegularExpression Id. */
+  int MULTI_LINE_COMMENT = 49;
 
   /** Lexical state. */
   int DEFAULT = 0;
+  /** Lexical state. */
+  int IN_SINGLE_LINE_COMMENT = 1;
+  /** Lexical state. */
+  int IN_FORMAL_COMMENT = 2;
+  /** Lexical state. */
+  int IN_MULTI_LINE_COMMENT = 3;
 
   /** Literal token values. */
   String[] tokenImage = {
@@ -76,36 +116,47 @@ public interface javinhaConstants {
     "\"*\"",
     "\"&&\"",
     "\"<\"",
+    "\"=\"",
+    "\"boolean\"",
+    "\"String\"",
+    "\"while\"",
+    "\"if\"",
+    "\"else\"",
     "\"lenght\"",
     "\"class\"",
+    "\"return\"",
+    "\"static\"",
+    "\"void\"",
+    "\"main\"",
     "\"public\"",
     "\"extends\"",
+    "\"System\"",
+    "\"out\"",
+    "\"println\"",
     "\"this\"",
+    "\"int\"",
     "\"new\"",
     "<INTEGER_LITERAL>",
     "<DIGIT>",
     "<BOOL>",
     "<ID>",
     "<LIT>",
-    "\"int\"",
+    "\";\"",
+    "\",\"",
+    "\".\"",
     "\"(\"",
     "\")\"",
     "\"[\"",
     "\"]\"",
-    "\"boolean\"",
-    "\"while\"",
-    "\"if\"",
-    "\"else\"",
-    "\";\"",
-    "\"{ public static void main ( String []\"",
     "\"{\"",
     "\"}\"",
-    "\"\"",
-    "\"return\"",
-    "\",\"",
-    "\"System.out.println\"",
-    "\"=\"",
-    "\".\"",
+    "\"//\"",
+    "<token of kind 45>",
+    "\"/*\"",
+    "<SINGLE_LINE_COMMENT>",
+    "\"*/\"",
+    "\"*/\"",
+    "<token of kind 50>",
     "\"!\"",
   };
 
